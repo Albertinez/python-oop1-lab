af8b1606c9df260cea3e0a44fb6648fd460689cc
Albertinez <albertmochama@gmail.com>
2026-02-26 18:22:01 +0300

    Fix Coffee and Book classes with __init__ and property setters

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,27 @@
-#!/usr/bin/env python3
+class Book:
+    def __init__(self, title, page_count):
+        self._title = title
+        self._page_count = None
+        self.page_count = page_count  # use setter for validation
 
-class Coffee:
-    pass
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        self._title = value
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
